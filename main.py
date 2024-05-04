ce247875e04675ffd590e74ea360557fe7af7059
JoseAT0912 <153239807+JoseAT0912@users.noreply.github.com>
2024-05-03 21:36:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+caract = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password = ""
+
+long = int( input("¿Cuántos carácteres?") )
+
+
+for i in range(long):
+    password += random.choice(caract)
+
+print(password)
